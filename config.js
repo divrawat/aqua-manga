83249fb302fbb959ec5b51f0c45aa57216ba4d9c
divrawat <divrawat2001@gmail>
2024-10-06 16:14:42 +0530

    sefv0esr5fg

---
+++ config.js
@@ -0,0 +1,34 @@
+
+// export const DOMAIN = 'http://localhost:3000';
+// export const BACKEND_DOMAIN = 'http://localhost:8000';
+
+export const DOMAIN = 'https://coffee-manga.online';
+export const BACKEND_DOMAIN = 'https://backend-mangawebsite.vercel.app'
+
+
+
+export const DOMAIN_NAME = 'coffee-manga.online';
+export const IMAGES_SUBDOMAIN = "https://photos.coffee-manga.online";
+export const DISQUS_SHORTNAME = "my-cms-7";
+
+
+export const APP_NAME = 'Coffee Manga';
+export const NavbarName = "Coffee Manga";
+
+export const APP_LOGO = `${IMAGES_SUBDOMAIN}/public/logo.webp`;
+export const NOT_FOUND_IMAGE = `${IMAGES_SUBDOMAIN}/public/404.webp`;
+export const FAVICON = `${IMAGES_SUBDOMAIN}/public/favicon.png`;
+
+export const navLinks = [
+    { text: 'New Chapters', href: `${DOMAIN}/new-chapters` },
+    { text: 'New Mangas', href: `${DOMAIN}/new-mangas` },
+    { text: 'Contact', href: `${DOMAIN}/contact` },
+];
+
+export const FooterLinks = [
+    { text: 'About', href: `${DOMAIN}/about` },
+    { text: 'Contact', href: `${DOMAIN}/contact` },
+    { text: 'Disclaimer', href: `${DOMAIN}/disclaimer` },
+    { text: 'Privacy Policy', href: `${DOMAIN}/privacy-policy` },
+    { text: 'Terms & Conditions', href: `${DOMAIN}/terms-and-conditions` }
+];
